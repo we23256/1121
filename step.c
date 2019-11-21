fde58da98d658a33ec18e169e551da4dcd874cbc
we23256 <55524007+we23256@users.noreply.github.com>
2019-11-21 15:35:30 +0900

    Add files via upload

---
+++ step.c
@@ -0,0 +1,27 @@
+ Yes, I did it!!!
+ 맞아, 내가 했어
+
+ I will do it
+ 내가할게
+
+ I can do it
+ 난 할수있어
+
+ I' ll try to do it
+ 내가 해볼게
+
+ How do i do it?
+ 어떻게 하지?
+
+ I want to do it
+ 난 그것을 하고 싶어
+
+ I can' t do it
+ 난 할수없어
+
+ I won' t do it
+ 난 원하지 않아
+
+ Which step have you reached today?
+ 오늘 어느 단계에 도달하셨나요?
+
